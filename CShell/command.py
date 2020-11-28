3781c021496f9a64bf1a1033fbafb7edb1b4cea8
QSoloX <qsolox@gmail.com>
2020-11-28 14:08:33 -0800

    Added in the base command file

---
+++ CShell/command.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Command:
+    '''The command object'''
+    def __init__(self, name, help_message, usage, func):
+        self.name = name
+        self.help_message = help_message
+        self.usage = usage
+        self.func = func
+
+
+def register(name, help_message, usage, commands):
+    '''Used to register different commands'''
+    def inner(func):
+        command = Command(name, help_message, usage, func)
+        commands[name] = command
+
+    return inner
