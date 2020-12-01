4a88b72eb9e7b5c47540edee2094be33c019a7d7
QSoloX <qsolox@gmail.com>
2020-12-01 07:45:42 -0800

    Added dep colorama for new release

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from setuptools import find_packages, setup
 setup(
     name='qshell',
     packages=find_packages(include=['qshell']),
-    version='0.1.0',
+    version='0.2.0',
     description='A interactive command shell wrapper from python projects',
     author='QSoloX',
     license='MIT',
-    install_requires=[],
+    install_requires=['colorama'],
     setup_requires=['pytest-runner'],
     test_suite='test',
 
